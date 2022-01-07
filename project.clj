df2c0b1f48f2e48f2c2ff1d81f6d16d2ce732968
NPException <npexception42@gmail.com>
2022-01-07 10:06:40 +0100

    create initial VM state

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject synacor-challenge "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.11.0-alpha3"]
+                 [criterium "0.4.6"]]
   ;; :main ^:skip-aot synacor-challenge.core
   :target-path "target/%s"
+  :repl-options {:init-ns synacor-challenge.core}
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
--- src/synacor_challenge/core.clj
+++ src/synacor_challenge/core.clj
@@ -1,2 +1,19 @@
-(ns synacor-challenge.core)
+(ns synacor-challenge.core
+  (:use [criterium.core]))
 
+(set! *unchecked-math* :warn-on-boxed)
+(set! *warn-on-reflection* true)
+
+;== architecture ==
+;- three storage regions
+;  - memory with 15-bit address space storing 16-bit values
+;  - eight registers
+;  - an unbounded stack which holds individual 16-bit values
+;- all numbers are unsigned integers 0..32767 (15-bit)
+;- all math is modulo 32768; 32758 + 15 => 5
+
+(def initial-state
+  {:ip        0
+   :memory    (vec (repeat 32768 0))                        ;; 15-bit address space
+   :registers [0 0 0 0 0 0 0 0]                             ;; 8 registers
+   :stack     '()})
